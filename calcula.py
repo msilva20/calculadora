39aa69dd9447050dee9e063244a58fb840d219fa
Marcelo Silva <97571500+msilva20@users.noreply.github.com>
2022-03-04 09:51:02 -0300

    Update calcula.py

--- calcula.py
+++ calcula.py
@@ -2,27 +2,27 @@
 while True:
     numero_1 = input('Digite um número: ')
     operador = input('Digite um operador matemático: ')
-    numero_3 = input('Digite outro número: ')
+    numero_2 = input('Digite outro número: ')
 
-    if not numero_1.isdigit() and not numero_3.isdigit():
+    if not numero_1.isdigit() and not numero_2.isdigit():
         print('Digite um número válido...')
         continue
         
 
     numero_1 = int(numero_1)
-    numero_3 = int(numero_3)
+    numero_2 = int(numero_2)
 
     if operador == '+':
-        print(numero_1 + numero_3)
+        print(numero_1 + numero_2)
         break 
     elif operador == '-':
-        print(numero_1 - numero_3)
+        print(numero_1 - numero_2)
         break
     elif operador == '*':
-        print(numero_1 * numero_3)
+        print(numero_1 * numero_2)
         break
     elif operador == '/':
-        print(numero_1 / numero_3)
+        print(numero_1 / numero_2)
         break
     else:
         print('Por favor digite um operador válido!')
